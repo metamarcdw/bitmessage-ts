92527ff0a5a02e8335294a93c4d005d78ed1c320
Marc Wood <marcdw87@gmail.com>
2019-09-09 06:23:49 -0400

    Create ISerializable

--- src/ProtocolObjects/index.ts
+++ src/ProtocolObjects/index.ts
@@ -1,10 +1,13 @@
 export const MAGIC = 0xE9BEB4D9;
 
-export interface IProtocolObject {
+export interface ISerializable {
+  serialize: () => Buffer;
+}
+
+export interface IProtocolObject extends ISerializable {
   head: null | number | IProtocolObject;
   body: number | string | Buffer |
     (number | string | Buffer | IProtocolObject)[];
-  serialize: () => Buffer;
 };
 
 export { IVarint, Varint } from './Varint';
