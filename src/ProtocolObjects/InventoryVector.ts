2c42a4d49af6af7bb18c491b06294f0e9338f11b
Marc Wood <marcdw87@gmail.com>
2019-09-10 22:01:20 -0400

    Create InventoryVector class

---
+++ src/ProtocolObjects/InventoryVector.ts
@@ -0,0 +1,18 @@
+import { ISerializable } from './';
+import { doubleSha512 } from '../util';
+
+export interface IInventoryVector extends ISerializable {
+  bmObject: ISerializable;
+}
+
+export class InventoryVector implements IInventoryVector {
+  public bmObject: ISerializable;
+
+  constructor (bmObject: ISerializable) {
+    this.bmObject = bmObject;
+  }
+
+  public serialize (): Buffer {
+    return doubleSha512(this.bmObject.serialize()).slice(0, 32);
+  }
+}
